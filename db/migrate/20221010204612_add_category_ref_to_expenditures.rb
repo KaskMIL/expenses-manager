7bd360c4c18560b8cbfbf7752787bb31a589e7f9
toMilanesi <tomi.milanesi@gmail.com>
2022-10-10 17:47:37 -0300

    Add expenditure reference category

---
+++ db/migrate/20221010204612_add_category_ref_to_expenditures.rb
@@ -0,0 +1,5 @@
+class AddCategoryRefToExpenditures < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :expenditures, :category_id, null: false, foreign_key: { to_table: 'category' }
+  end
+end
