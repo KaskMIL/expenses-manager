0fb549ca73941a5bb99273ce06e388dfb9646367
toMilanesi <tomi.milanesi@gmail.com>
2022-10-10 17:50:08 -0300

    Fix expenditures references categories migration

--- db/migrate/20221010204612_add_category_ref_to_expenditures.rb
+++ db/migrate/20221010204612_add_category_ref_to_expenditures.rb
@@ -1,5 +1,5 @@
 class AddCategoryRefToExpenditures < ActiveRecord::Migration[7.0]
   def change
-    add_reference :expenditures, :category_id, null: false, foreign_key: { to_table: 'category' }
+    add_reference :expenditures, :category_id, null: false, foreign_key: { to_table: 'categories' }
   end
 end
