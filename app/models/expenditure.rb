d318e82c048349fca42b663abf80af8b9e965ebb
toMilanesi <tomi.milanesi@gmail.com>
2022-10-10 19:04:23 -0300

    Add expenditure validations

--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -1,4 +1,9 @@
 class Expenditure < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   belongs_to :category, foreign_key: 'category_id'
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :amount, presence: true
+  validate :amount, numericality: { only_integer: true, greater_than: 0 }
 end
