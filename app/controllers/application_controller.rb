cf2c2ee045b1e7226d396f7bc560475031895c08
toMilanesi <tomi.milanesi@gmail.com>
2022-10-11 15:22:30 -0300

    Fix after sign out path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
     devise_parameter_sanitizer.permit(:sign_in, keys: %i[email password])
   end
+
+  private
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 end
