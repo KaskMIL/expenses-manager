75ccf6faa22057948718456e9144465b541a621c
toMilanesi <tomi.milanesi@gmail.com>
2022-10-11 12:57:32 -0300

    Add pretect_from_forgery appController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :config_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :null_session
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def config_permitted_parametters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
     devise_parameter_sanitizer.permit(:sign_in, keys: %i[email password])
   end
