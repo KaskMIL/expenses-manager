2e0cf4e0f492c2d7505449843c4423963bf85a82
toMilanesi <tomi.milanesi@gmail.com>
2022-10-11 12:35:26 -0300

    Add and install devise, add permitted_parameters applicationController

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :expenditures
   resources :categories
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'categories#index'
 end
